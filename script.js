0ba0f7769857b96b00d3450f25e02304913dd25f
bilboandyoda <jordboudreau@gmail.com>
2017-11-24 23:16:22 -0800

    event listener practice

--- script.js
+++ script.js
@@ -1,36 +1,44 @@
-var movies = [
-		{
-			name:"Dark Knight", 
-			rating: 4.5, 
-			hasWatched: true
-		},	
-		{
-			name:"Fantastic Mr. Fox", 
-			rating: 5, 
-			hasWatched: true
-		},	
-		{
-			name:"Matrix 2", 
-			rating: 3, 
-			hasWatched: false
-		},	
-		{
-			name:"Dark Knight", 
-			rating: 4.5, 
-			hasWatched: true
-		},
-];
+// var movies = [
+// 		{
+// 			name:"Dark Knight", 
+// 			rating: 4.5, 
+// 			hasWatched: true
+// 		},	
+// 		{
+// 			name:"Fantastic Mr. Fox", 
+// 			rating: 5, 
+// 			hasWatched: true
+// 		},	
+// 		{
+// 			name:"Matrix 2", 
+// 			rating: 3, 
+// 			hasWatched: false
+// 		},	
+// 		{
+// 			name:"Dark Knight", 
+// 			rating: 4.5, 
+// 			hasWatched: true
+// 		},
+// ];
 
-movies.forEach(function(movie) {
-	var result = "You have ";
-	if(movie.hasWatched === true){
-		result += "watched ";
-	}
-	else if(movie.hasWatched === false){
-		result += "not seen ";
-	}
+// movies.forEach(function(movie) {
+// 	var result = "You have ";
+// 	if(movie.hasWatched === true){
+// 		result += "watched ";
+// 	}
+// 	else if(movie.hasWatched === false){
+// 		result += "not seen ";
+// 	}
 
-	result+= movie.name + " - " + movie.rating + " stars";
+// 	result+= movie.name + " - " + movie.rating + " stars";
 
-	console.log(result);
+// 	console.log(result);
+// });
+
+var hOne = document.querySelector("h1");
+var paragraph = document.querySelector("p");
+
+paragraph.addEventListener("click", function() {
+	hOne.textContent = "booger";
 });
+
